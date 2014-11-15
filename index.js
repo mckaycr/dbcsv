19876c05e0d6f6ec171b2846f2698a902e185f2a
Thomas Leen <tl@thomasleen.com>
2014-11-15 09:43:35 -0500

    using csv2array to read data

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 'use strict';
 
-var pkg = require('./package.json');
+// read only for now (till I have a need to write)
 
-module.exports = function(config){
+var csv2array = require('csv2array'),
+fs = require('fs'),
+pkg = require('./package.json'),
+_ = require('lodash');
+
+module.exports = function(){
+
+  if(arguments.length === 0) throw new Error('cannot call without arguments');
+
+  // -- Configuration --
+  var config;
+  if(_.isString(arguments[0])) config = {filename : arguments[0]};
+  else if(_.isObject(arguments[0])) config = arguments[0];
+
+  if(_.isUndefined(config)) throw new Error('filename string or configuration object required');
+
+  var configuration = _.defaults({}, config, {
+    encoding : 'utf8',
+    headers : true
+  });
+  if(!configuration.filename) throw new Error('filename required');
   
-  var data = [];
+  // Get CSV File
+  if(!fs.existsSync(configuration.filename)) throw new Error('Cannot find file: ' + configuration.filename);
 
-  function length(){
-    return data.length;
-  }
+  var data = csv2array(
+    fs.readFileSync(configuration.filename, {encoding : configuration.encoding}),
+    {
+      headers : configuration.headers
+    });
+  console.log(data);
   
   return {
-    length : length,
+    size : _.size(data),
     version : pkg.version
   };
 
--- test/index.js
+++ test/index.js
@@ -3,12 +3,57 @@
 /* global describe, it, should */
 /* jshint expr: true */
 
-var dbcsv = require('../');
+var dbcsv = require('../'),
+path = require('path'),
+pkg = require('../package.json');
 
-var pkg = require('../package.json');
+var filename = path.resolve(__dirname, './sample.csv');
+
+describe('creation signatures', function(){
+
+  it('should throw error on empty', function(){
+    (function(){ 
+      dbcsv(); 
+    }).should.throw('cannot call without arguments');
+  });
+
+  it('should throw error if not string or object', function(){
+    [4, 1.0, false, true, undefined, null].forEach(function(val){
+      (function(){ 
+	dbcsv(val); 
+      }).should.throw('filename string or configuration object required');
+    });
+  });
+
+  it('should throw error if configuration filename does not exist', function(){
+    (function(){ 
+      dbcsv({}); 
+    }).should.throw('filename required');
+  });
+});
+
+describe('creation filename', function(){
+
+  it('should throw error on non-existent file', function(){
+    (function(){ 
+      dbcsv('./missingfilename.csv'); 
+    }).should.throw(/missingfilename/);
+
+    (function(){ 
+      dbcsv({filename : './missingfilename.csv'}); 
+    }).should.throw(/missingfilename/);
+  });
+
+}); 
 
 describe('version()', function(){
   it('should have same version as package', function(){
-    pkg.version.should.equal(dbcsv().version);
+    pkg.version.should.equal(dbcsv(filename).version);
+  });
+});
+
+describe('size()', function(){
+  it('sample data size', function(){
+    dbcsv(filename).size.should.eql(4);
   });
 });
