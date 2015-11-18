a6f2af80c883cf911ca2da4b3f6628d13333194c
Tom Leen <tml@leentop.thomasleen.com>
2015-11-18 14:30:41 -0500

    Revert "Merge pull request #1 from mckaycr/master"
    Breaks underlying implementation.

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ module.exports = function(source, configuration){
     encoding : 'utf8',
     headers : true,
     trim : true,
-    headersLower : true,
-    separator: ','
+    headersLower : true
   });
 
   // -- Get Data --
@@ -25,7 +24,7 @@ module.exports = function(source, configuration){
   // we read the file contents and csv2array does the actual parsing-to-array work
   var data = csv2array(
     fs.readFileSync(c.source, {encoding : c.encoding}),
-    {separator:c.separator});  
+    {});  
 
   // -- Deal With Headers --
 
